dae69fec77f2c780bc1959a0c224cce24c589f8a
Rishi-2323 <joshirishi23@gmail.com>
2022-02-01 16:15:55 +0530

    test done

--- prediction_service/prediction.py
+++ prediction_service/prediction.py
@@ -76,6 +76,11 @@ def api_response(dict_request):
             response = predict(data)
             response = {"response" : response}
             return response
-    except Exception as e:
+
+    except NotInRange as e:
         response = {"the_execpted_range": get_schema(), "response":str(e)}
+        return response
+    
+    except NotInCols as e:
+        response = {"the_execpted_cols": get_schema().keys(), "response":str(e)}
         return response
